736cb080656f1ac971c96407d1dbf58fe89c9e1c
kenny <sambalah.kenny@gmail.com>
2026-01-26 21:33:34 +0800

    Fix duplicate page definition warning

--- guest_management/guest_management.py
+++ guest_management/guest_management.py
@@ -1,10 +1,12 @@
 import reflex as rx
-from guest_management.guest_management.pages.home import home
+
+from guest_management.pages.home_page import home
 
 
 def index() -> rx.Component:
-    return home()
+    return rx.box(rx.redirect("/home"))
 
 
 app = rx.App()
 app.add_page(index)
+app.add_page(home, route="/home")
--- guest_management/pages/home_page.py
+++ guest_management/pages/home_page.py
@@ -1,12 +1,11 @@
 import reflex as rx
 
 
-@rx.page
-def feature_card(icon: str, title: str, description: str) -> rx.Component:
+def feature_card(icon_tag: str, title: str, desc: str):
     return rx.vstack(
-        rx.icon(tag=icon, size=30, color="blue"),
+        rx.icon(tag=icon_tag, size=30, color="blue"),
         rx.heading(title, size="4"),
-        rx.text(description, color_override="gray", text_align="center"),
+        rx.text(desc, color_override="gray", text_align="center"),
         padding="2em",
         border="1px solid #e5e7eb",
         border_radius="15px",
@@ -35,7 +34,7 @@ def home() -> rx.Component:
             rx.hstack(
                 rx.vstack(
                     rx.heading("Manage Guest faster wiht Us", size="9"),
-                    rx.text("No manual seacrhing, its fully automated", size="5"),
+                    rx.text("No manual seacrhing, its", size="5"),
                     rx.button("Start Now", size="3", variant="soft"),
                     spacing="5",
                     padding_y="10vh",
